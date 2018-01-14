de6d7a668196bd7d92b1d5a922a767517a5f6cfc
CDF Sven <chendf999@gmail.com>
2018-01-13 18:14:31 -0800

    html setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBnb0YC39pJe1wg6OU86oI1-fvVWm0mMCY",
+  authDomain: "rps-multiplayer-dc.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-dc.firebaseio.com",
+  projectId: "rps-multiplayer-dc",
+  storageBucket: "rps-multiplayer-dc.appspot.com",
+  messagingSenderId: "76580510550"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
